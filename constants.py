ae2aa64b47cf8d9705f244a654e22587ddc6e203
Christopher El-Murr <christopher.el-murr@stu.ocsb.ca>
2026-01-16 02:11:38 +0000

    added code.py

---
+++ constants.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# Created By: Christopher El-Murr
+# Date: 11 08, 2025
+# this program is the constants file for the "Space Aliens" game for the PyBadge
+
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TILE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+TOTAL_NUMBER_OF_LASERS = 8
+SHIP_SPEED = 1
+LASER_SPEED = 2
+ALIEN_SPEED = 1
+OFF_SCREEN_X = -100
+OFF_SCREEN_Y = -100
+OFF_TOP_SCREEN = -1 * SPRITE_SIZE
+OFF_BOTTOM_SCREEN = SCREEN_Y + SPRITE_SIZE
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
+
+# Using for button state
+button_state = {
+    "button_up": "up",
+    "button_just_pressed": "just_pressed",
+    "button_still_pressed": "still_pressed",
+    "button_released": "released",
+}
+
+RED_PALLETTE = (
+    b"\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+    b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+)
